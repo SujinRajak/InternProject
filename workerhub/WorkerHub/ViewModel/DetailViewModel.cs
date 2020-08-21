23c1ca0546e6cf29085f3a6156b3582bcc930608
sujin rajak <sujinrajak@gmail.com>
2020-08-21 18:37:54 +0545

    working with home section

---
+++ workerhub/WorkerHub/ViewModel/DetailViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+using WorkerHub.Models;
+
+namespace WorkerHub.ViewModel
+{
+    public class DetailViewModel
+    {
+        public IEnumerable<ApplicationUser> users;
+        public IEnumerable<UserAcademic> userAcad { get; }
+
+        public IEnumerable<UserSkills> UserSkills { get; }
+
+        public IEnumerable<UserExperience> userExperiences { get; }
+    }
+}
