07d5fcf3fe8fb1445af51de59e22631edb54023b
Mani <Mani.mhzn.182@gmail.com>
2024-07-01 19:28:12 +0545

    added request controller

--- WorkerHub/WorkerHub/Controllers/EmployeeDetailPermissionController.cs
+++ WorkerHub/WorkerHub/Controllers/EmployeeDetailPermissionController.cs
@@ -1,7 +1,10 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
 using WorkerHub.Interface;
 using WorkerHub.Models;
+using WorkerHub.Service;
 
 namespace WorkerHub.Controllers
 {
@@ -11,21 +14,39 @@ namespace WorkerHub.Controllers
 		private readonly IApplicationUser _context;
 		private readonly ApplicationDbContext _dbcontext;
 		private readonly IEmployeeDetailPermissionService _employeeDetailPermissionService;
+		private readonly IPaymentService _paymentService;
 
 		public EmployeeDetailPermissionController(UserManager<ApplicationUser> userManager,
 			IApplicationUser context,
 			ApplicationDbContext dbcontext,
-			IEmployeeDetailPermissionService employeeDetailPermissionService)
+			IEmployeeDetailPermissionService employeeDetailPermissionService,
+			IPaymentService paymentService)
 		{
 			_userManager = userManager;
 			_context = context;
 			_dbcontext = dbcontext;
 			_employeeDetailPermissionService = employeeDetailPermissionService;
+			_paymentService = paymentService;
 		}
 
 		public IActionResult Index()
 		{
 			return View();
+		}
+		public async Task<bool> Request(string employeeId)
+		{
+			ApplicationUser user = _context.getUser(_userManager.GetUserId(User));
+
+			if (await _paymentService.CheckIfUserIsSubscribed(new Guid(user.Id)))
+			{
+				await _employeeDetailPermissionService.RequestProfileAccess(employeeId, user.Id);
+				return true;
+			}
+			return false;
+		}
+		public async Task<bool> Update(Guid id, bool status)
+		{
+			return(await _employeeDetailPermissionService.UpdateProfileAccessRequest(id, status));
 		}
 	}
 }
