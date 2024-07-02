51e63fba97584157ac397b4d3041bc0733a26aea
Mani <Mani.mhzn.182@gmail.com>
2024-07-02 23:15:46 +0545

    added list view for request

--- WorkerHub/WorkerHub/Controllers/EmployeeDetailPermissionController.cs
+++ WorkerHub/WorkerHub/Controllers/EmployeeDetailPermissionController.cs
@@ -1,10 +1,13 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Threading.Tasks;
 using WorkerHub.Interface;
 using WorkerHub.Models;
 using WorkerHub.Service;
+using System.Linq;
+using WorkerHub.ViewModel;
 
 namespace WorkerHub.Controllers
 {
@@ -29,22 +32,50 @@ namespace WorkerHub.Controllers
 			_paymentService = paymentService;
 		}
 
-		public IActionResult Index()
-		{
-			return View();
-		}
-		public async Task<bool> Request(string employeeId)
+        public async Task<IActionResult> Index(int? page)
+        {
+            int pageSize = 10; // Number of items per page
+            int pageNumber = page ?? 1;
+
+
+            var permissions = await (
+             from permission in _dbcontext.EmployeeDetailPermissions
+             join hiringManager in _dbcontext.applicationUser on permission.HiringManagerId equals hiringManager.Id
+             join employee in _dbcontext.Users on permission.EmployeeId equals employee.Id
+             orderby permission.ExpiresIn descending
+             select new EmployeePermissionViewModel
+             {
+                 Id = permission.Id,
+                 HiringManagerName = hiringManager.UserName,
+                 EmployeeName = employee.UserName,
+                 Status = permission.Status,
+                 ExpiresIn = permission.ExpiresIn
+             }
+         )
+         .Skip((pageNumber - 1) * pageSize)
+         .Take(pageSize)
+         .ToListAsync();
+
+            int pageCount = (int)Math.Ceiling((double)permissions.Count / pageSize);
+
+            ViewBag.PageNumber = pageNumber;
+            ViewBag.PageCount = pageCount;
+
+            return View(permissions);
+        }
+        public async Task<bool> AccessRequest(string id)
 		{
 			ApplicationUser user = _context.getUser(_userManager.GetUserId(User));
 
 			if (await _paymentService.CheckIfUserIsSubscribed(new Guid(user.Id)))
 			{
-				await _employeeDetailPermissionService.RequestProfileAccess(employeeId, user.Id);
+				await _employeeDetailPermissionService.RequestProfileAccess(id, user.Id);
 				return true;
 			}
 			return false;
 		}
-		public async Task<bool> Update(Guid id, bool status)
+        [HttpPost]
+        public async Task<bool> Update(Guid id, bool status)
 		{
 			return(await _employeeDetailPermissionService.UpdateProfileAccessRequest(id, status));
 		}
---
+++ WorkerHub/WorkerHub/ViewModel/EmployeePermissionViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace WorkerHub.ViewModel
+{
+    public class EmployeePermissionViewModel
+    {
+        public Guid Id { get; set; }
+        public string HiringManagerName { get; set; }
+        public string EmployeeName { get; set; }
+        public string Status { get; set; }
+        public DateTime ExpiresIn { get; set; }
+    }
+}
